556619c0b0f5d1c8376f0aff7a6e19ef56239bae
Merily Roopärg <merily.r@gmail.com>
2023-03-20 19:54:11 +0200

    ANET-DO-1 Docker compose for database and backend

--- API/CityApp-ANET/DAL.App.EF/AppDbContext.cs
+++ API/CityApp-ANET/DAL.App.EF/AppDbContext.cs
@@ -1,15 +1,16 @@
 ﻿using CityApp_ANET.Models;
 using Microsoft.EntityFrameworkCore;
 
-namespace CityApp_ANET.DAL.App.EF;
-
-public class AppDbContext : DbContext
+namespace CityApp_ANET.DAL.App.EF
 {
-    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    public class AppDbContext : DbContext
     {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<City> Cities { get; set; } = null!;
+
+        public DbSet<User> Users { get; set; } = null!;
     }
-
-    public DbSet<City> Cities { get; set; } = null!;
-
-    public DbSet<User> Users { get; set; } = null!;
 }
